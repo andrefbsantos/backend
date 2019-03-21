17cc6b64bc4ce09246475ad97d40d0590d64e4cb
Joel Reis <jreis@imaginarycloud.com>
2019-03-21 23:39:40 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config({ path: 'variables.env' });
+
+const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
+
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers: {
+    Query,
+    Mutation,
+  },
+  context: req => ({
+    ...req,
+    prisma,
+  }),
+});
+
+// You can add options to your server, check the GraphQL-Yoga API
+const options = {
+  port: process.env.PORT,
+  endpoint: '/graphql',
+  playground: '/playground',
+}
+
+server.start(options, ({ port }) => console.log(`Server is running on http://localhost:${port}`))
