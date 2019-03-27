fbbcf94c112bad08b010e6f81873afada1c5740c
Bruno Sousa <bsousa@imaginaycloud.com>
2019-03-27 11:50:10 +0000

    Add new queries and mutations for Hops, Beer, Grains

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,21 @@
 const Mutation = {
+  createGrain(parent, args, ctx) {
+    return ctx.prisma.createGrain({
+        ...args.data
+    })
+  },
+  createHops(parent, args, ctx) {
+    return ctx.prisma.createHops({
+        ...args.data
+    })
+  },
+  updateHops(parent, args, ctx) {
+    console.log(args)
+    return ctx.prisma.updateHops({
+        data: {...args.data},
+        where: { ...args.where}
+    })
+  }
 }
 
 module.exports = Mutation;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,16 @@
 
 const Query = {
+  hops(parent, args, ctx) {
+    return ctx.prisma.hops({
+      ...args.where
+    })
+  },
+  hopses(parent, args, ctx) {
+    return ctx.prisma.hopses()
+  },
+  grains(parent, args, ctx) {
+    return ctx.prisma.grains()
+  }
 }
 
 module.exports = Query;
